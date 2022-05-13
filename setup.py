43d8f7c2c2adfc484c5209c7cbed3d508b764755
Merengo <marwamerengo@gmail.com>
2022-05-13 05:44:22 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup,find_packages
+
+with open("README", 'r') as f:
+    long_description = f.read()
+
+setup(
+   name='youtube',
+   version='1.0',
+   description='A useful module',
+   license="MIT",
+   long_description=long_description,
+   author='Man Foo',
+   author_email='marwamerengo@gmail.com',
+   url="",
+   packages=find_packages(),  #same as name
+   install_requires=[], #external packages as dependencies
+   scripts=[
+            'scripts/cool',
+            'scripts/skype',
+           ]
+)
